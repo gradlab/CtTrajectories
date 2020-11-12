290094a44bf529226806ad3578c4db9f1324f996
Stephen Kissler <skissler@hsph.harvard.edu>
2020-11-12 08:58:06 -0500

    Got esttimate of the number infected at an event working too.

--- code/analysis/development.R
+++ code/analysis/development.R
@@ -20,3 +20,30 @@ ggplot(data=tibble(x=-t_test,y=eff_se), aes(x=x, y=y)) +
   theme(text=element_text(size=18))
 
 
+pop_pars <- c(
+        n_attendees=500,
+        prev=0.05
+        )
+
+
+ninf <- reduce(lapply(t_test, get_n_infectious, 
+	lod=35, se=0.95, inf_ct=inf_ct, maxct=maxct, params_df_slice, pop_pars=pop_pars, event_duration=3/24), bind_rows) %>% 
+	pivot_wider(names_from="statistic", values_from=c("value")) %>%
+	mutate(mean_smooth=predict(loess(mean~t, data=., span=0.6))) %>%
+	mutate(lwr_smooth=predict(loess(lwr~t, data=., span=0.6))) %>%
+	mutate(upr_smooth=predict(loess(upr~t, data=., span=0.6))) %>%
+	mutate(t=-t)
+
+ggplot() + 
+	geom_ribbon(
+	  data=ninf, 
+	  aes(x=t, ymin=lwr_smooth, ymax=upr_smooth), alpha=0.2, fill="grey") + 
+	geom_point(data=ninf, aes(x=t, y=lwr), size=0.1, alpha=0) +
+	geom_point(data=ninf, aes(x=t, y=upr), size=0.1, alpha=0) +
+	geom_point(data=ninf, aes(x=t, y=mean), size=0.1, alpha=0) + 
+	geom_line(data=ninf, aes(x=t, y=mean), stat="smooth", method="loess", span=0.6) + 
+	coord_cartesian(ylim=c(0,max(ninf$upr)), expand=FALSE) + 
+	scale_x_reverse() + 
+	theme_minimal() + 
+	theme(text=element_text(size=18)) + 
+	labs(title="Number infectious at event", subtitle="(90% pred. interval)", x="Days prior to event", y="Number infectious at event")
